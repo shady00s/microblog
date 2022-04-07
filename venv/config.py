853291c980c2d74c615529587135d48f2d50097d
shady00s <ssk2310@hotmail.com>
2022-04-07 14:50:10 +0200

    Database added

--- venv/config.py
+++ venv/config.py
@@ -1,4 +1,8 @@
-import os 
-
+import os
+import sqlite3 
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECERT_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICAIONS = False
